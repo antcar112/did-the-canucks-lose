05e8dc8a1a7f8a98bf8e884227b4f7b5a51ac33d
Anthony Caron <antcar1992@gmail.com>
2021-02-08 23:27:20 -0800

    less spaghetti

--- src/useLoss.ts
+++ src/useLoss.ts
@@ -4,17 +4,16 @@ const URL = 'https://statsapi.web.nhl.com/api/v1/teams/23?expand=team.schedule.p
 
 export const useLoss = () => {
   const [loading, setLoading] = useState(true)
-  const [won, setWon] = useState(true)
+  const [won, setWon] = useState(false)
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch(URL)
       const body = await res.json()
       const { away, home } = body.teams[0].previousGameSchedule.dates[0].games[0].teams
-      const vanIsAway = away.team.id === 23 ? true : false
-      const winnerIsAway = away.score > home.score ? true : false
-      const vanIsWinner = (vanIsAway && winnerIsAway) || (!vanIsAway && !winnerIsAway)
-      setWon(vanIsWinner)
+      const vanIsAway = away.team.id === 23
+      const winnerIsAway = away.score > home.score
+      setWon((vanIsAway && winnerIsAway) || (!vanIsAway && !winnerIsAway))
       setLoading(false)
     }
     fetchData()
